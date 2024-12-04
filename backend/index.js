c67da8fc4858443bc96e433c9a751867f5fe11af
Lakshit Bagul <lakshitbagul777@gmail.com>
2024-12-04 19:59:13 +0530

    Created new repository and initial commit with backend completed

---
+++ backend/index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const { createTodo, updateTodo } = require('./types');
+const { todo } = require('./db');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.post('/todo', async function(req,res){
+    const createPayload = req.body;
+    const parsePayload = createTodo.safeParse(createPayload);
+
+    if(!parsePayload.success){
+        res.status(411).json({
+            msg: "You sent the wrong inputs"
+        })
+        return;
+    }
+
+    await todo.create({
+        title: createPayload.title,
+        description: createPayload.description
+    })
+
+    res.json({
+        msg: "Todo created"
+    })
+
+})
+app.get('/todos', async function(req,res){
+    const todos = await todo.find({});
+
+    res.json({
+        todos
+    });
+})
+app.put('/completed', async function(req,res){
+    const updatePayload = req.body;
+    const parsePayload = updateTodo.safeParse(updatePayload);
+    if(!parsePayload.success){
+        res.status(411).json({
+            msg: "You sent the wrong inputs"
+        })
+        return;
+    }
+
+    await todo.updateOne({
+        _id: req.body.id
+    },{
+        completed: true
+    });
+
+    res.json({
+        msg: "Todo marked as completed!"
+    });
+})
+
+app.listen(PORT,()=>console.log(`Server listening on port ${PORT}`));
---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      hi there
+    </div>
+  )
+}
+
+export default App
